3fb5c3286aa77469b974f7f1c05ae3bde0fbcb67
Ondřej Hošek <ondrej.hosek@tuwien.ac.at>
2023-03-28 20:27:16 +0200

    finish listing zip files

--- libzmx/src/lib.rs
+++ libzmx/src/lib.rs
@@ -13,7 +13,10 @@ use std::io::{Read, Seek, SeekFrom};
 
 use io_ext::ReadExt;
 
-use crate::zip_format::EndOfCentralDirectory;
+use crate::zip_format::{
+    CentralDirectoryHeader, EndOfCentralDirectory, Zip64EndOfCentralDirectory,
+    Zip64EndOfCentralDirectoryLocator,
+};
 
 
 /// An error that may occur during ZIP decoding or encoding.
@@ -84,6 +87,17 @@ fn lookback_for_signature<F: Read + Seek>(mut file: F, signature: u32) -> Result
     }
 }
 
+fn best_effort_decode(bytes: &[u8]) -> String {
+    match String::from_utf8(Vec::from(bytes)) {
+        Ok(s) => s,
+        Err(_) => {
+            bytes.iter()
+                .map(|b| char::from_u32(*b as u32).unwrap())
+                .collect()
+        },
+    }
+}
+
 
 /// Obtains the list of file names in the archive.
 pub fn zip_get_files<F: Read + Seek>(mut zip_file: F) -> Result<Vec<Vec<u8>>, Error> {
@@ -99,6 +113,59 @@ pub fn zip_get_files<F: Read + Seek>(mut zip_file: F) -> Result<Vec<Vec<u8>>, Er
 
     // read EoCD
     let eocd = EndOfCentralDirectory::read_after_signature(&mut zip_file)?;
+    if eocd.disk_no != 0 {
+        return Err(Error::SpannedArchive);
+    }
+    let mut zip64_central_directory_loc: Option<u64> = None;
+    if eocd.should_check_zip64() {
+        // go back to EoCD start
+        lookback_for_signature(&mut zip_file, EndOfCentralDirectory::signature())?;
 
-    todo!();
+        // try to find Zip64 EoCD locator
+        let zip64_eocd_loc_found = lookback_for_signature(&mut zip_file, Zip64EndOfCentralDirectoryLocator::signature())?;
+        if zip64_eocd_loc_found {
+            let zip64_eocd_loc = Zip64EndOfCentralDirectoryLocator::read_after_signature(&mut zip_file)?;
+            if zip64_eocd_loc.disk_no != 0 || zip64_eocd_loc.total_disks != 1 {
+                return Err(Error::SpannedArchive);
+            }
+
+            // try to find Zip64 EoCD
+            zip_file.seek(SeekFrom::Start(zip64_eocd_loc.offset_on_disk))?;
+
+            // try to read Zip64 EoCD
+            let zip64_eocd_sig = zip_file.read_u32_le()?;
+            if zip64_eocd_sig == Zip64EndOfCentralDirectory::signature() {
+                let zip64_eocd = Zip64EndOfCentralDirectory::read_after_signature(&mut zip_file)?;
+                if zip64_eocd.total_central_dir_entries != zip64_eocd.total_central_dir_entries_this_disk {
+                    return Err(Error::SpannedArchive);
+                }
+                zip64_central_directory_loc = Some(zip64_eocd.central_dir_offset_on_disk);
+            }
+        }
+    }
+    let central_directory_loc: u64 = if let Some(zcdl) = zip64_central_directory_loc {
+        zcdl
+    } else {
+        if eocd.total_central_dir_entries != eocd.total_central_dir_entries_this_disk {
+            return Err(Error::SpannedArchive);
+        }
+        eocd.central_dir_offset_on_disk.into()
+    };
+    zip_file.seek(SeekFrom::Start(central_directory_loc))?;
+
+    // now we can read out the files
+    let mut file_names = Vec::new();
+    loop {
+        let file_header_loc = zip_file.seek(SeekFrom::Current(0))?;
+        let signature = zip_file.read_u32_le()?;
+        if signature != CentralDirectoryHeader::signature() {
+            break;
+        }
+        let cdh = CentralDirectoryHeader::read_after_signature(&mut zip_file)?;
+        let file_name = best_effort_decode(&cdh.file_name);
+        println!("{:?} ({})", file_name, cdh.uncompressed_size);
+        file_names.push(cdh.file_name.clone());
+    }
+
+    Ok(file_names)
 }
--- libzmx/src/zip_format.rs
+++ libzmx/src/zip_format.rs
@@ -46,6 +46,7 @@ impl EndOfCentralDirectory {
 
     const fn min_len_bias() -> u64 {
         4 // signature
+        + 2 // comment length
     }
 
     /// Write the end-of-central-directory record.
@@ -105,6 +106,20 @@ impl EndOfCentralDirectory {
             central_dir_offset_on_disk,
             comment,
         })
+    }
+
+    /// Whether to check for Zip64 records as well.
+    ///
+    /// The ZIP specification states that if at least one of the fields in this record is greater
+    /// than its maximum value (0xFFFF or 0xFFFFFFFF, depending on the field size), the information
+    /// is to be extracted from the equivalent Zip64 record instead.
+    pub const fn should_check_zip64(&self) -> bool {
+        self.disk_no == u16::MAX
+        || self.start_central_dir_disk_no == u16::MAX
+        || self.total_central_dir_entries_this_disk == u16::MAX
+        || self.total_central_dir_entries == u16::MAX
+        || self.central_directory_size == u32::MAX
+        || self.central_dir_offset_on_disk == u32::MAX
     }
 }
 
@@ -290,7 +305,7 @@ pub(crate) struct CentralDirectoryHeader {
     pub general_purpose_bit_flag: u16,
 
     /// Method with which the file was compressed.
-    pub compression_method: u32,
+    pub compression_method: u16,
 
     /// The file's time of last modification.
     pub last_mod_file_time: u16,
@@ -363,7 +378,7 @@ impl CentralDirectoryHeader {
         writer.write_u16_le(self.creator_version)?;
         writer.write_u16_le(self.required_version)?;
         writer.write_u16_le(self.general_purpose_bit_flag)?;
-        writer.write_u32_le(self.compression_method)?;
+        writer.write_u16_le(self.compression_method)?;
         writer.write_u16_le(self.last_mod_file_time)?;
         writer.write_u16_le(self.last_mod_file_date)?;
         writer.write_u32_le(self.crc32)?;
@@ -388,15 +403,10 @@ impl CentralDirectoryHeader {
     ///
     /// It is assumed that the reader is positioned after the signature.
     pub fn read_after_signature<R: Read>(mut reader: R) -> Result<Self, crate::Error> {
-        let signature_bytes = reader.read_u32_le()?;
-        if signature_bytes != Self::signature() {
-            return Err(crate::Error::IncorrectSignature);
-        }
-
         let creator_version = reader.read_u16_le()?;
         let required_version = reader.read_u16_le()?;
         let general_purpose_bit_flag = reader.read_u16_le()?;
-        let compression_method = reader.read_u32_le()?;
+        let compression_method = reader.read_u16_le()?;
         let last_mod_file_time = reader.read_u16_le()?;
         let last_mod_file_date = reader.read_u16_le()?;
         let crc32 = reader.read_u32_le()?;
@@ -445,20 +455,19 @@ impl CentralDirectoryHeader {
 ///
 /// This is one of the possible fields in a central directory entry's
 /// [`extra_fields`](CentralDirectoryHeader::extra_fields).
-#[minimum_length]
 #[derive(Clone, Debug, Default, Eq, Hash, Ord, PartialEq, PartialOrd)]
 pub(crate) struct Zip64ExtraField {
     /// The uncompressed size of this file.
-    pub uncompressed_size: u64,
+    pub uncompressed_size: Option<u64>,
 
     /// The compressed size of this file.
-    pub compressed_size: u64,
+    pub compressed_size: Option<u64>,
 
     /// Relative offset to the local file header.
-    pub local_header_relative_offset: i64,
+    pub local_header_relative_offset: Option<i64>,
 
     /// The number of the disk containing the first chunk of this file.
-    pub disk_number_start: u32,
+    pub disk_number_start: Option<u32>,
 }
 impl Zip64ExtraField {
     /// The tag for this extra field.
@@ -470,13 +479,27 @@ impl Zip64ExtraField {
         writer.write_u16_le(Self::tag())?;
 
         // write size (except tag field and size field)
-        writer.write_u16_le(Self::min_len().try_into().unwrap())?;
+        let size =
+            if self.uncompressed_size.is_some() { 8 } else { 0 }
+            + if self.compressed_size.is_some() { 8 } else { 0 }
+            + if self.local_header_relative_offset.is_some() { 8 } else { 0 }
+            + if self.disk_number_start.is_some() { 4 } else { 0 }
+        ;
+        writer.write_u16_le(size)?;
 
         // write fields
-        writer.write_u64_le(self.uncompressed_size)?;
-        writer.write_u64_le(self.compressed_size)?;
-        writer.write_i64_le(self.local_header_relative_offset)?;
-        writer.write_u32_le(self.disk_number_start)?;
+        if let Some(uncompressed_size) = self.uncompressed_size {
+            writer.write_u64_le(uncompressed_size)?;
+        }
+        if let Some(compressed_size) = self.compressed_size {
+            writer.write_u64_le(compressed_size)?;
+        }
+        if let Some(local_header_relative_offset) = self.local_header_relative_offset {
+            writer.write_i64_le(local_header_relative_offset)?;
+        }
+        if let Some(disk_number_start) = self.disk_number_start {
+            writer.write_u32_le(disk_number_start)?;
+        }
 
         Ok(())
     }
@@ -485,16 +508,58 @@ impl Zip64ExtraField {
     ///
     /// It is assumed that its tag has just been read (to ensure the read function of the correct
     /// field is called).
-    pub fn read_after_tag<R: Read>(mut reader: R) -> Result<Self, crate::Error> {
+    ///
+    /// The relevant values from the central directory entry must be passed, as the extra field only
+    /// contains those values that are out-of-range (and, therefore, have their max value).
+    pub fn read_after_tag<R: Read>(
+        mut reader: R,
+        cdir_uncompressed_size: u32,
+        cdir_compressed_size: u32,
+        cdir_local_header_relative_offset: i32,
+        cdir_disk_number_start: u16,
+
+    ) -> Result<Self, crate::Error> {
+        let mut expected_length = 0;
+        if cdir_uncompressed_size == u32::MAX {
+            expected_length += 8;
+        }
+        if cdir_compressed_size == u32::MAX {
+            expected_length += 8;
+        }
+        // the relevant APPNOTE.TXT section only mentions 0xFFFF and 0xFFFFFFFF;
+        // in signed (two's complement) arithmetic, this is equivalent to -1
+        if cdir_local_header_relative_offset == -1 {
+            expected_length += 8;
+        }
+        if cdir_disk_number_start == u16::MAX {
+            expected_length += 4;
+        }
+
         let length = reader.read_u16_le()?;
-        if u64::from(length) != Self::min_len() {
+        if u64::from(length) != expected_length {
             return Err(crate::Error::UnexpectedExtraDataLength(length));
         }
 
-        let uncompressed_size = reader.read_u64_le()?;
-        let compressed_size = reader.read_u64_le()?;
-        let local_header_relative_offset = reader.read_i64_le()?;
-        let disk_number_start = reader.read_u32_le()?;
+        let uncompressed_size = if cdir_uncompressed_size == u32::MAX {
+            Some(reader.read_u64_le()?)
+        } else {
+            None
+        };
+        let compressed_size = if cdir_compressed_size == u32::MAX {
+            Some(reader.read_u64_le()?)
+        } else {
+            None
+        };
+        let local_header_relative_offset = if cdir_local_header_relative_offset == -1 {
+            Some(reader.read_i64_le()?)
+        } else {
+            None
+        };
+        let disk_number_start = if cdir_disk_number_start == u16::MAX {
+            Some(reader.read_u32_le()?)
+        } else {
+            None
+        };
 
         Ok(Self {
             uncompressed_size,
