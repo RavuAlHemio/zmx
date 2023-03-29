327a4e849441ed67fc481442079a306242baef1a
Ondřej Hošek <ondra.hosek@gmail.com>
2023-03-29 23:42:35 +0200

    winzmx: window class preparations

--- winzmx/src/main.rs
+++ winzmx/src/main.rs
@@ -7,23 +7,27 @@ use std::process::ExitCode;
 
 use windows::w;
 use windows::core::PWSTR;
-use windows::Win32::Foundation::{HWND, LPARAM, WPARAM};
-use windows::Win32::UI::WindowsAndMessaging::{MB_ICONERROR, MB_OK, MessageBoxW};
+use windows::Win32::Foundation::{HWND, LPARAM, LRESULT, WPARAM};
+use windows::Win32::Graphics::Gdi::{COLOR_WINDOW, HBRUSH};
+use windows::Win32::System::LibraryLoader::GetModuleHandleW;
+use windows::Win32::UI::WindowsAndMessaging::{
+    DefWindowProcW, HCURSOR, HICON, MB_ICONERROR, MB_OK, MessageBoxW, WNDCLASSEXW, WNDCLASS_STYLES,
+};
 use windows::Win32::UI::Controls::Dialogs::{
-    OFN_ALLOWMULTISELECT,
-
     GetOpenFileNameW, OFN_ENABLESIZING, OFN_EXPLORER, OFN_HIDEREADONLY, OFN_PATHMUSTEXIST,
     OPENFILENAMEW,
 };
 
 
-unsafe extern "system" fn hook_func(_hdlg: HWND, _ui_msg: u32, _wparam: WPARAM, _lparam: LPARAM) -> usize {
-    0
+extern "system" fn wnd_proc(hwnd: HWND, msg: u32, wparam: WPARAM, lparam: LPARAM) -> LRESULT {
+    unsafe { DefWindowProcW(hwnd, msg, wparam, lparam) }
 }
 
 
 fn main() -> ExitCode {
     let args: Vec<OsString> = env::args_os().collect();
+
+    // find out which file we're trying to analyze
     let file_path = if args.len() == 1 {
         let mut file_name_buffer = vec![0u16; 32768];
 
@@ -34,11 +38,7 @@ fn main() -> ExitCode {
         ofnw.lpstrFile = PWSTR::from_raw(file_name_buffer.as_mut_ptr());
         ofnw.nMaxFile = file_name_buffer.len().try_into().unwrap();
         ofnw.lpstrTitle = w!("WinZMX: Open");
-        ofnw.Flags =
-            OFN_ENABLESIZING | OFN_ALLOWMULTISELECT | OFN_EXPLORER | OFN_HIDEREADONLY
-            | OFN_PATHMUSTEXIST
-        ;
-        ofnw.lpfnHook = Some(hook_func);
+        ofnw.Flags = OFN_ENABLESIZING | OFN_EXPLORER | OFN_HIDEREADONLY | OFN_PATHMUSTEXIST;
         let success = unsafe { GetOpenFileNameW(&mut ofnw) };
         if !success.as_bool() {
             return ExitCode::FAILURE;
@@ -59,6 +59,22 @@ fn main() -> ExitCode {
         };
         return ExitCode::FAILURE;
     };
+
+    let instance = unsafe { GetModuleHandleW(None) }
+        .expect("failed to obtain my own instance");
+
+    // define a window class
+    let mut window_class = WNDCLASSEXW::default();
+    window_class.cbSize = size_of_val(&window_class).try_into().unwrap();
+    window_class.style = WNDCLASS_STYLES::default();
+    window_class.lpfnWndProc = Some(wnd_proc);
+    window_class.hInstance = instance;
+    window_class.hIcon = HICON::default();
+    window_class.hCursor = HCURSOR::default();
+    window_class.hbrBackground = HBRUSH(isize::try_from(COLOR_WINDOW.0).unwrap() + 1);
+    window_class.lpszClassName = w!("WinZMX-MainWindow");
+
+    // TODO
 
     ExitCode::SUCCESS
 }
